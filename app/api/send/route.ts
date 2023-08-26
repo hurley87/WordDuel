3ba6d60464983ffe7d379e5ecfe51aeed96a5827
David Hurley <dhurls99@gmail.com>
2023-08-26 14:12:23 -0400

    refactor api key

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,7 +4,9 @@ import { Resend } from 'resend';
 
 export const runtime = 'nodejs';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const apiKey = process.env.RESEND_API_KEY as string;
+
+const resend = new Resend(apiKey);
 
 export async function POST() {
   try {
