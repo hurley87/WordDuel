a981ae8129573e34fce30bac994f2d6375d3dcdb
David Hurley <dhurls99@gmail.com>
2023-09-13 06:46:02 -0400

    try this bug fix

--- hooks/useWrite.ts
+++ hooks/useWrite.ts
@@ -3,11 +3,12 @@ import { ethers } from 'ethers';
 import { magic } from '@/lib/magic';
 
 export const useWrite = () => {
-  if (!magic.rpcProvider) throw new Error('Magic not initialized');
-  const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
-  const signer = provider.getSigner();
-  const address = process.env
-    .NEXT_PUBLIC_DUELS_CONTRACT_ADDRESS as `0x${string}`;
-  const abi = Duels.abi;
-  return new ethers.Contract(address, abi, signer);
+  if (magic.rpcProvider) {
+    const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
+    const signer = provider.getSigner();
+    const address = process.env
+      .NEXT_PUBLIC_DUELS_CONTRACT_ADDRESS as `0x${string}`;
+    const abi = Duels.abi;
+    return new ethers.Contract(address, abi, signer);
+  }
 };
