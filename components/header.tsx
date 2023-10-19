3d4c7493a951e6a5dc4338c84fc6cdec7b031933
David Hurley <dhurls99@gmail.com>
2023-10-19 12:12:58 -0400

    UI tweak on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -44,58 +44,62 @@ function Header() {
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
-              <DialogTitle>How To Play</DialogTitle>
+              <DialogTitle className="text-left">How To Play</DialogTitle>
               <DialogDescription>
-                <p>You and your opponent take turns guessing the right word.</p>
-                <ul className="list-disc pt-4 list-inside">
-                  <li>Each guess must be a valid 5-letter word.</li>
-                  <li>Letter colors will give you hints.</li>
-                </ul>
-                <div className="flex flex-col gap-4 py-4 block">
-                  <p className="font-bold">Examples</p>
-                  <div className="flex gap-1 text-lg">
-                    <div className="border-2 border-green-500 py-1 px-2 bg-green-500 text-white">
-                      W
-                    </div>
-                    <div className="border-2 border-accent py-1 px-2">E</div>
-                    <div className="border-2 border-accent py-1 px-2">A</div>
-                    <div className="border-2 border-accent py-1 px-2">R</div>
-                    <div className="border-2 border-accent py-1 px-2">Y</div>
-                  </div>
+                <div className="text-left">
                   <p>
-                    <b>W</b> is in the word and in the correct spot.
+                    You and your opponent take turns guessing the right word.
                   </p>
-                  <div className="flex gap-1 text-lg">
-                    <div className="border-2 border-accent py-1 px-2">P</div>
-                    <div className="border-2 border-yellow-500 bg-yellow-500 text-white py-1 px-2">
-                      I
+                  <ul className="list-disc pt-4 list-inside">
+                    <li>Each guess must be a valid 5-letter word.</li>
+                    <li>Letter colors will give you hints.</li>
+                  </ul>
+                  <div className="flex flex-col gap-4 py-4 block">
+                    <p className="font-bold">Examples</p>
+                    <div className="flex gap-1 text-lg">
+                      <div className="border-2 border-green-500 py-1 px-2 bg-green-500 text-white">
+                        W
+                      </div>
+                      <div className="border-2 border-accent py-1 px-2">E</div>
+                      <div className="border-2 border-accent py-1 px-2">A</div>
+                      <div className="border-2 border-accent py-1 px-2">R</div>
+                      <div className="border-2 border-accent py-1 px-2">Y</div>
                     </div>
-                    <div className="border-2 border-accent py-1 px-2">L</div>
-                    <div className="border-2 border-accent py-1 px-2">L</div>
-                    <div className="border-2 border-accent py-1 px-2">S</div>
+                    <p>
+                      <b>W</b> is in the word and in the correct spot.
+                    </p>
+                    <div className="flex gap-1 text-lg">
+                      <div className="border-2 border-accent py-1 px-2">P</div>
+                      <div className="border-2 border-yellow-500 bg-yellow-500 text-white py-1 px-2">
+                        I
+                      </div>
+                      <div className="border-2 border-accent py-1 px-2">L</div>
+                      <div className="border-2 border-accent py-1 px-2">L</div>
+                      <div className="border-2 border-accent py-1 px-2">S</div>
+                    </div>
+                    <p>
+                      <b>I</b> is in the word but in the wrong spot.
+                    </p>
+                    <div className="flex gap-1 text-lg">
+                      <div className="border-2 border-accent py-1 px-2">V</div>
+                      <div className="border-2 border-accent py-1 px-2">A</div>
+                      <div className="border-2 border-accent py-1 px-2">G</div>
+                      <div className="border-2 border-gray-500 bg-gray-500 text-white py-1 px-2">
+                        U
+                      </div>
+                      <div className="border-2 border-accent py-1 px-2">E</div>
+                    </div>
+                    <p>
+                      <b>U</b> is not in the word in any spot.
+                    </p>
                   </div>
                   <p>
-                    <b>I</b> is in the word but in the wrong spot.
-                  </p>
-                  <div className="flex gap-1 text-lg">
-                    <div className="border-2 border-accent py-1 px-2">V</div>
-                    <div className="border-2 border-accent py-1 px-2">A</div>
-                    <div className="border-2 border-accent py-1 px-2">G</div>
-                    <div className="border-2 border-gray-500 bg-gray-500 text-white py-1 px-2">
-                      U
-                    </div>
-                    <div className="border-2 border-accent py-1 px-2">E</div>
-                  </div>
-                  <p>
-                    <b>U</b> is not in the word in any spot.
+                    Whover guesses the right word first wins the the game. If
+                    you play for ETH, each player transfers ETH for each guess.
+                    The winner is transferred all the ETH. If the game is a
+                    draw, the pot is split evenly.
                   </p>
                 </div>
-                <p>
-                  Whover guesses the right word first wins the the game. If you
-                  play for ETH, each player transfers ETH for each guess. The
-                  winner is transferred all the ETH. If the game is a draw, the
-                  pot is split evenly.
-                </p>
               </DialogDescription>
             </DialogHeader>
           </DialogContent>
