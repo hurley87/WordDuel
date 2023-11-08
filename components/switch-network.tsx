37d199c96291bc96a5e6f5c1dd46ce1fb5e584c7
David Hurley <dhurls99@gmail.com>
2023-11-08 09:44:04 -0500

    redo switch network

--- components/switch-network.tsx
+++ components/switch-network.tsx
@@ -4,10 +4,17 @@ import '@/styles/globals.css';
 
 import { baseGoerli, base } from 'wagmi/chains';
 import { useWallets } from '@privy-io/react-auth';
-import { useEffect } from 'react';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { Icons } from './icons';
+import { usePrivyWagmi } from '@privy-io/wagmi-connector';
+import { toast } from './ui/use-toast';
 
 function SwitchNetwork({ children }) {
   const { wallets } = useWallets();
+  const { wallet: w, ready } = usePrivyWagmi();
+  const [isLoading, setIsLoading] = useState(false);
+  const wallet = wallets.find((wallet) => wallet.address === w?.address);
 
   const chainId =
     process.env.NODE_ENV === 'production'
@@ -15,23 +22,46 @@ function SwitchNetwork({ children }) {
       : (baseGoerli.id as number);
 
   async function handleSwitchNetwork() {
-    // setIsLoading(true);
+    setIsLoading(true);
     const embeddedWallet = wallets.find(
       (wallet) => wallet.walletClientType === 'privy'
     );
 
-    if (embeddedWallet) {
-      await embeddedWallet?.switchChain(chainId);
-    } else {
-      await wallets[0]?.switchChain(chainId);
+    try {
+      if (embeddedWallet) {
+        await embeddedWallet?.switchChain(chainId);
+      } else {
+        await wallets[0]?.switchChain(chainId);
+      }
+    } catch (e) {
+      const description = e?.message || 'Something went wrong.';
+      setIsLoading(false);
+      toast({
+        title: 'Error switching network.',
+        description,
+        variant: 'destructive',
+      });
     }
   }
 
-  useEffect(() => {
-    handleSwitchNetwork();
-  }, []);
-
-  return <div>{children}</div>;
+  return (
+    <div>
+      {ready && chainId.toString() !== wallet?.chainId.split(':')[1] && (
+        <div className="fixed inset-0 z-50 bg-white/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-slate-950/80">
+          <div className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-slate-200 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full dark:border-slate-800 dark:bg-slate-950 text-center">
+            <h1>Connect to the Base network.</h1>
+            <Button onClick={handleSwitchNetwork}>
+              {isLoading && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Connect
+            </Button>
+          </div>
+        </div>
+      )}
+      {children}
+    </div>
+  );
 }
 
 export default SwitchNetwork;
