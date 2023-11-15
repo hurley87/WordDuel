e81163c639a14c039b686fe60ad32ca849c00900
David Hurley <dhurls99@gmail.com>
2023-11-15 08:59:06 -0500

    remove container component

--- components/container.tsx
+++
@@ -1,7 +0,0 @@
-export const Container = ({ children }) => {
-  return (
-    <div className="'flex items-center justify-center [&>div]:w-full">
-      {children}
-    </div>
-  );
-};
--- components/duel-cancelled.tsx
+++ components/duel-cancelled.tsx
@@ -1,16 +1,15 @@
-import { Container } from './container';
 import { Card, CardDescription, CardHeader } from './ui/card';
 
 export const DuelCancelled = () => {
   return (
     <div className="flex flex-col gap-2 max-w-lg mx-auto">
-      <Container>
+      <div className="'flex items-center justify-center [&>div]:w-full">
         <Card>
           <CardHeader>
             <CardDescription>This game has been cancelled.</CardDescription>
           </CardHeader>
         </Card>
-      </Container>
+      </div>
     </div>
   );
 };
