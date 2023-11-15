4252b5433ab959bdcda0cf3d08b8e2ef90defd91
David Hurley <dhurls99@gmail.com>
2023-11-15 12:59:14 -0500

    fix invalid guess bug

--- components/wordle.tsx
+++ components/wordle.tsx
@@ -116,6 +116,7 @@ export default function Wordle() {
           description: 'Please try again.',
           variant: 'destructive',
         });
+        setIsLoading(false);
         return {
           status: 'playing',
         };
