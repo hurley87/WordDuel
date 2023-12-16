b986e9a0707778f1504e09df5e4dfd26c5377f81
David Hurley <dhurls99@gmail.com>
2023-12-16 16:29:20 -0500

    add transfer ETH out of account

---
+++ app/transfer/page.tsx
@@ -0,0 +1,9 @@
+import { TransferForm } from '@/components/transfer-form';
+
+export default function TransferPage() {
+  return (
+    <div className="max-w-md mx-auto pt-20 flex flex-col gap-10">
+      <TransferForm />
+    </div>
+  );
+}
--- components/transfer-form.tsx
+++ components/transfer-form.tsx
@@ -8,12 +8,8 @@ import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { toast } from '@/components/ui/use-toast';
 import { Icons } from '@/components/icons';
-import {
-  useBalance,
-  usePrepareSendTransaction,
-  useSendTransaction,
-} from 'wagmi';
-import { parseEther, parseGwei } from 'viem';
+import { useBalance } from 'wagmi';
+import { parseEther } from 'viem';
 import { usePrivyWagmi } from '@privy-io/wagmi-connector';
 import {
   Card,
@@ -22,32 +18,53 @@ import {
   CardFooter,
   CardHeader,
 } from './ui/card';
+import { usePrivy } from '@privy-io/react-auth';
+import { base, baseGoerli } from 'viem/chains';
 
 export function TransferForm() {
+  const { sendTransaction } = usePrivy();
+
   const [to, setTo] = React.useState('');
   const [amount, setAmount] = React.useState('');
-  const { config } = usePrepareSendTransaction({
-    to,
-    value: parseEther(amount),
-    gasPrice: parseGwei('20'),
-  });
+  const [isLoading, setIsLoading] = React.useState(false);
 
-  const { data, isLoading, isSuccess, sendTransaction } =
-    useSendTransaction(config);
   const { wallet: activeWallet } = usePrivyWagmi();
   const { data: balance } = useBalance({
     address: activeWallet?.address as `0x${string}`,
   });
 
-  function handleTransfer() {
+  async function handleTransfer() {
+    setIsLoading(true);
+
+    const chainId =
+      process.env.NODE_ENV === 'production' ? base.id : baseGoerli.id;
+    const unsignedTx = {
+      to,
+      chainId,
+      value: parseEther(amount),
+    };
+
+    console.log(unsignedTx);
+
+    // Replace this with the text you'd like on your transaction modal
+    const uiConfig = {
+      header: 'ETH Transfer',
+      description: 'Transfer ETH to another wallet',
+      buttonText: 'Transfer',
+    };
+
     try {
-      sendTransaction?.();
+      const txReceipt = await sendTransaction(unsignedTx, uiConfig);
+
+      console.log(txReceipt);
+
       toast({
         title: 'Success',
         description: `${amount} ETH transferred to ${formatAddress(to)}.`,
       });
       setTo('');
       setAmount('');
+      setIsLoading(false);
     } catch (e) {
       const description = (e as Error)?.message || 'Please try again.';
       return toast({
